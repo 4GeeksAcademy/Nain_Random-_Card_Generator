3f1712658f0e28ee0baa748db6ab471c3767976e
LinseNain <141765188+LinseNain@users.noreply.github.com>
2023-12-11 17:39:25 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector(`#card`).classList.add(generateRandomSuit());
+  document.querySelector(`.number`).innerHTML = generateRandomNumbers();
+
+  function generateRandomNumbers() {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "K",
+      "Q"
+    ];
+    let indexnumb = Math.floor(Math.random() * numbers.length);
+    return numbers[indexnumb];
+  }
+  function generateRandomSuit() {
+    let suit = ["trebol", "dinamond", "heart", "picas"];
+    let indexSuit = Math.floor(Math.random() * suit.length);
+    return suit[indexSuit];
+  }
+
+  document.querySelector(`#card`);
   console.log("Hello Rigo from the console!");
 };
